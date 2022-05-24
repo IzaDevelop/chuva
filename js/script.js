31e9fe123ce7748b7904f36a3fd522be48f45d17
Izaart <izabelle.sdn@gmail.com>
2022-05-24 02:06:17 -0300

    alternancia em discussoes, sidebar, html

--- js/script.js
+++ js/script.js
@@ -12,3 +12,71 @@ if(pontos.style.display === "none"){
     maisTexto.style.display = 'inline';
     btnVerMais.innerHTML = '<i>Ver menos</i>';
 }}
+
+// esconder conteudo discussões
+$(document).ready(function(){
+	$('#botao').on('click', function(){
+    	$('#conteudo').toggle();
+    });
+});
+
+// mostrar form discussões
+$(document).ready(function(){
+	$('#botao').on('click', function(){
+    	$('#formulario').toggle();
+    });
+});
+
+// criando comentario dinamico
+const formulario = document.querySelector("form");
+const campoAssunto = document.querySelector("#assunto");
+const campoMensagem = document.querySelector("#mensagem");
+const comentario = document.querySelector("#comentado")
+
+formulario.addEventListener("submit", function(event){
+    // cancelar redirecionamento do form
+    event.preventDefault();
+    // capturar o valor digitado nos campos
+    let assunto = campoAssunto.value;
+    let mensagem = campoMensagem.value;
+    // criar dinamicamente via js
+    // criar tags
+    let titulo = document.createElement("h4");
+    let paragrafo = document.createElement("p");
+    // montar como deve ser exibido o conteudo
+    titulo.innerHTML = `${assunto}`;
+    paragrafo.innerHTML = `${mensagem}`;
+    // exibindo o conteudo na tag
+    comentario.appendChild(titulo);
+    comentario.appendChild(paragrafo);
+    // resetar o formulário
+    formulario.reset();
+});
+
+// mostrar atualizacoes discussões
+$(document).ready(function(){
+	$('#enviar').on('click', function(){
+    	$('#atualizar').toggle();
+    });
+});
+
+// mostrar novo tópico discussões
+ $(document).ready(function(){
+	$('#enviar').on('click', function(){
+    	$('#form').toggle();
+    });
+});
+
+// italico e negrito no textarea
+const negrito = document.querySelector("#negrito");
+const italico = document.querySelector('#italico');
+const pagina = document.querySelector("textarea");
+negrito.addEventListener("click", function(event){
+    event.preventDefault();
+    pagina.classList.toggle("negrito");
+});
+
+italico.addEventListener("click", function(event){
+    event.preventDefault();
+    pagina.classList.toggle("italico");
+});
--- js/sidebar.js
+++ js/sidebar.js
@@ -1,14 +1,21 @@
-/* $(document).ready(function () {
-
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
-    });
-
-}); */
-
-$(document).ready(function () {
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
-        $(this).toggleClass('active');
+$(document).ready(function(){
+	$('.closebtn').on('click', function(){
+    	$('.menu').toggle();
     });
 });
+
+$(document).ready(function(){
+	$('.openbtn').on('click', function(){
+    	$('.menu').toggle();
+    });
+});
+
+function openNav() {
+  document.querySelector("#Sidebar").style.width = "200px";
+  document.querySelector("#global").style.marginLeft = "200px";
+}
+
+function closeNav() {
+  document.querySelector("#Sidebar").style.width = "0";
+  document.querySelector("#global").style.marginLeft = "0";
+}
